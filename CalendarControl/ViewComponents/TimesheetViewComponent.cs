9259664c6e4c23331e2b3c419fb11c1c252b31d8
Conrad Bekondo <philllittle302@gmail.com>
2022-04-13 18:33:34 +0100

    first draft

---
+++ CalendarControl/ViewComponents/TimesheetViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using CalendarControl.ViewComponents.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarControl.ViewComponents
+{
+    public class TimesheetViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(TimesheetControlViewModel? model = null)
+        {
+            model ??= new TimesheetControlViewModel()
+            {
+                DisplayMode = DisplayMode.Day
+            };
+            return await RenderComponentAsync(model);
+        }
+
+        private async Task<IViewComponentResult> RenderComponentAsync(TimesheetControlViewModel viewModel)
+        {
+            await Task.CompletedTask;
+            return View(viewModel);
+        }
+    }
+}
---
+++ CalendarControl/ViewComponents/ViewModels/TimesheetControlViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CalendarControl.ViewComponents.ViewModels
+{
+    public enum DisplayMode
+    {
+        Day, WorkWeek, CalendarWeek, Next7Days, Month, List
+    }
+
+    public class TimesheetControlViewModel
+    {
+        public DisplayMode DisplayMode { get; set; }
+    }
+}
